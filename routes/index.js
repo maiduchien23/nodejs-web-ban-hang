781d4c701739b86637612ccd6f19f125017a97fa
maiduchien23 <mdhien2302@gmail.com>
2024-03-04 16:11:45 +0700

    day 1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const homeController = require("../controller/homeController");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+router.get("/", homeController.index);
 
 module.exports = router;
